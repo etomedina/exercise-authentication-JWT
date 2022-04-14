ba779f06c0acec54164504eb8973ccde4454d601
Tulio Lara <telara2801@gmail.com>
2022-04-14 12:44:56 -0400

    :tada: first commit

--- src/main.py
+++ src/main.py
@@ -6,19 +6,25 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from sqlalchemy import true
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_jwt_extended  import JWTManager,create_access_token, get_jwt_identity, jwt_required
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY']= os.environ.get('FLASK_API_KEY')
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+jwt=JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -39,7 +45,61 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/signup', methods=['POST'])
+def create_user():
+    body=request.json
+    user=User.create(
+        name=body["name"],
+        email=body["email"],
+        password=body["password"]
+    )
+    if user is not None:
+        db.session.add(user)
+        try:
+            db.session.commit()
+            return jsonify(user.serialize()),201
+        except Exception as error:  
+            db.session.rollback()
+            return jsonify({
+                "msg":"One error happened during record data"
+            }),500
+    else:
+        return jsonify({"msg":"Please check your data, there are some mistake"}),400
+
+
+    # print(body)
+    # print(request)
+    # return jsonify(body)
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    body=request.json
+    user_login=User.login(
+        body["email"],body["password"]
+    )
+    if user_login:
+        access_token=create_access_token(identity=user_login.id)
+        print (access_token)
+        return jsonify({ "token": access_token, "user_id": user_login.id }),201
+        ##return jsonify(access_token),201  #201 de create
+    else:
+        return jsonify({"msg":"Please check your data, there are some mistake"}),400 
+
+@app.route("/private", methods=["GET"])
+@jwt_required()
+def protected():
+    user_id=get_jwt_identity()
+    user=User.query.get(user_id)
+    return jsonify(user.serialize()),200
+
+
+
+
+
+
+
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,12 +1,14 @@
+#from msilib.schema import InstallUISequence
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(200), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(280), unique=False, nullable=False)
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +16,29 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            "password":self.password
             # do not serialize the password, its a security breach
         }
+    @classmethod
+    def create(cls,name,email,password):
+        instance =cls(
+            name=name,
+            email=email,
+            password=password
+        )
+        if isinstance(instance,cls):
+            return instance 
+        else:
+            return None    
+    
+    @classmethod
+    def login(cls,email1,password):
+        user=cls.query.filter_by(email=email1).one_or_none()
+        if (not isinstance(user,cls)):
+            return user
+        if user.password==password:
+            return user
+        else:
+            return False       
